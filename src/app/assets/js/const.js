f51c730ce92a4cb72ceb80565cb17031629d0605
dongnguyentat <dong.nguyen@mycleo.net>
2024-08-05 22:19:26 +0700

    fix

--- src/app/assets/js/const.js
+++ src/app/assets/js/const.js
@@ -1,5 +1,5 @@
-﻿var cartURL = "https://ohp.com.vn/gio-hang";
-var apiURL= "https://ohp.com.vn/api/";
+﻿var cartURL = "https://ohp.vn/gio-hang";
+var apiURL= "https://ohp.vn/api/";
 var webUrlLogin = "http://admin.ohp.vn/dang-nhap";
 var ndt_vnd = 3760;
 var serviceFee = 10000;
