c6f99f55b813a6c6c0a6f62c0594be2a017c8245
Dong Nguyen Tat <dong.nguyen@mycleo.net>
2024-08-08 14:35:29 +0700

    fix

--- src/app/assets/js/const.js
+++ src/app/assets/js/const.js
@@ -1,5 +1,5 @@
 ﻿var cartURL = "https://ohp.vn/gio-hang";
 var apiURL= "https://ohp.vn/api/";
-var webUrlLogin = "http://admin.ohp.vn/dang-nhap";
+var webUrlLogin = "https://ohp.vn/dang-nhap";
 var ndt_vnd = 3760;
 var serviceFee = 10000;
